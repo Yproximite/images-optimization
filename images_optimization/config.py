d6fe0f0028426c1c522acdace2511d1d613fa15f
Hugo Alliaume <kocal@live.fr>
2018-05-18 16:10:01 +0200

    feat: implement config in JSON, with config parser

--- images_optimization/config.py
+++ images_optimization/config.py
@@ -1,3 +1,7 @@
+import json
 import os
 
 root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+with open(os.path.join(root_path, 'config.json')) as config_file:
+    config = json.load(config_file)
--- images_optimization/mozjpeg.py
+++ images_optimization/mozjpeg.py
@@ -1,6 +1,10 @@
+import shutil
+import tempfile
+
+from images_optimization.config import config
 from images_optimization.execute import execute
 
-binary = 'cjpeg'
+binary = config.get('cjpeg_binary')
 
 
 def check_binary():
