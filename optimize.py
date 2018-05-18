802049c83ace124542a75d5b75f516788749ca25
Hugo Alliaume <kocal@live.fr>
2018-05-18 11:29:48 +0200

    Implement args parsing + checks on working directory

--- optimize.py
+++ optimize.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import argparse
+import os
 import subprocess
 
 from images_optimization import mozjpeg
@@ -7,7 +9,30 @@ from images_optimization.logger import logger
 
 
 def run():
+    args = _parse_args()
+    directory = os.path.realpath(args.directory)
+
+    _check_working_directory(directory)
     _check_mozjpeg_binary()
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser(description='Images optimization')
+    parser.add_argument('directory')
+    args = parser.parse_args()
+
+    return args
+
+
+def _check_working_directory(directory):
+    if os.path.exists(directory) and os.path.isdir(directory):
+        logger.info('Working directory: %s' % directory)
+    elif os.path.isfile(directory):
+        logger.error('Working directory is not a valid directory (%s)' % directory)
+        exit(2)
+    else:
+        logger.error('Working directory does not exist (%s)' % directory)
+        exit(2)
 
 
 def _check_mozjpeg_binary():
