d95a9cfe1bc04955b11c03794b51d3add40e95d5
Hugo Alliaume <kocal@live.fr>
2018-05-18 13:39:37 +0200

    feat: implement files glob + checks on extension + prepare optimizers

---
+++ images_optimization/optimizer.py
@@ -0,0 +1,28 @@
+import glob
+
+from images_optimization.logger import logger
+
+jpg_extensions = ['jpg', 'jpeg']
+extensions = jpg_extensions + ['png', 'gif']
+
+
+def optimize_images_from_directory(directory):
+    for filename in glob.iglob('%s/*.*' % directory, recursive=True):
+        filename_lower = filename.lower()
+        is_image = any([filename_lower.endswith('.%s' % extension) for extension in extensions])
+        is_jpg = any([filename_lower.endswith('.%s' % jpg_extension) for jpg_extension in jpg_extensions])
+
+        if is_jpg:
+            optimize_jpeg(filename)
+        elif is_image:
+            optimize_non_jpeg_image(filename)
+        else:
+            pass  # skip non-images
+
+
+def optimize_jpeg(filename):
+    logger.info('Optimizing %s with mozjpeg...' % filename)
+
+
+def optimize_non_jpeg_image(filename):
+    logger.info('Optimizing %s with Pillow...' % filename)
--- optimize.py
+++ optimize.py
@@ -4,6 +4,7 @@ import os
 
 from images_optimization.args_parser import parse_args
 from images_optimization.checkers import check_working_directory, check_mozjpeg_binary
+from images_optimization.optimizer import optimize_images_from_directory
 
 
 def run():
@@ -12,6 +13,7 @@ def run():
 
     check_working_directory(directory)
     check_mozjpeg_binary()
+    optimize_images_from_directory(directory)
 
 
 if __name__ == '__main__':
